ae5b4b41a71a090376f29ce87eff436a2068f448
apixtunr <alvaro.pixtunramirez@gmail.com>
2024-11-01 13:37:43 -0600

    despliegue

---
+++ frontendProject/frontalm14/.angular/cache/17.3.8/vite/deps/@angular_material_button.js
@@ -0,0 +1,32 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-LVXF7RFH.js";
+import "./chunk-GKUJN6ZN.js";
+import "./chunk-PN7SR372.js";
+import "./chunk-ZHPRS2ED.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
