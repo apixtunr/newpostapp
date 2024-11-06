5e4fd877baf64c431d02b85ba37c00aa4ef807f9
Apix <147033703+apixtunr@users.noreply.github.com>
2024-11-06 13:48:25 -0600

    Update CorsConfig.java

--- backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/config/CorsConfig.java
+++ backendProject/alm14/src/main/java/com/dbproject/pixtun/alm14/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5545")
+                        .allowedOrigins("http://vps-4501634-x.dattaweb.com:5545")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
